5fabced6bd2d35cc67425a67c1645bae20c8da70
unknown <ana80202>
2024-01-22 17:42:46 +0900

    수업

--- 3_JAVAscript/JS/03_연산자.js
+++ 3_JAVAscript/JS/03_연산자.js
@@ -211,11 +211,26 @@ function check() {
 
      console.log(a, b, c);
 
-
-
 }
 
 
+
+function printJSObject() {
+
+    const userName = document.getElementById("userName");
+
+    const userAge = document.getElementById("userAge");
+
+    const userGender = document.getElementById("userGender");
+
+    const userInfo = {
+        이름 : userName.value,
+        나이 : userAge.value,
+        성별 : userGender.value
+    };
+
+    console.log(userInfo);
+ }
 
 
 
