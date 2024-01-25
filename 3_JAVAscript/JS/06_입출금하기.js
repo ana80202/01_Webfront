a086d2ae3a242b10b6e0f29974574c25ccaafb7f
unknown <ana80202>
2024-01-25 17:30:31 +0900

    수업

--- 3_JAVAscript/JS/06_입출금하기.js
+++ 3_JAVAscript/JS/06_입출금하기.js
@@ -51,43 +51,48 @@ function deposit() {
 
 
 
-//출금  ???
-function withdrwal() {
 
-    if(amount.value.length == 0){
+
+// 출금
+function withdrawal() {
+
+    if(amount.value.length == 0) {
         alert("금액을 입력해주세요");
+
     } else {
-        
+
         const password = prompt("비밀번호를 입력하세요");
 
         if(password == null) {
-           alert("취소");
+            alert("취소");
+        
+        }else {
 
-        } else {
-            //비밀번호가 일치하지 않을 경우
-            if( password != '1234') {                           ///????
+            // 비밀번호가 일치하지 않을 경우
+            if(password != '1234') {
                 alert("비밀번호가 일치하지 않습니다");
-            } else {
-                //비밀번호가 일치하는 경우
+
+            } else { // 비밀번호가 일치하는 경우
+
                 const money = Number(amount.value);
 
-                //출금할 금액이 잔액보다 큰 경우 
+                // 출금할 금액이 잔액 보다 큰 경우
                 if(money > balance) {
                     alert("출금 금액이 잔액보다 클 수 없습니다");
-                
-                } else {//출금할 금액이 잔액보다 작거나 같은 경우
+
+                } else { // 출금할 금액이 잔액보다 작거나 같은 경우
+
                     //balance = balance - money;
                     balance -= money;
                     output.innerText = balance;
-                    amount.value = ''; //input에 작성된 값 제거
+                    amount.value = ''; // input 에 작성된 값 제거
 
-                    alert(`${money}원이 출금되었습니다. 남은 잔액 ${balance}`);
-
+                    alert(`${money}원이 출금 되었습니다. 남은 잔액 ${balance}`);
                 }
             }
+
         }
     }
-
 
 }
 
