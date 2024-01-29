c483247e4f86e1a5352c46591dad42ae74acac0d
unknown <ana80202>
2024-01-29 17:59:27 +0900

    수업

---
+++ 3_JAVAscript/JS/시험.js
@@ -0,0 +1,42 @@
+const endFn = document.getElementById("#endFn")
+const w = document.querySelector("#w")
+const h = document.querySelector("#h")
+const f = document.querySelector("#f")
+const text = document.getElementById("#text")
+const bg = document.getElementById("#bg")
+const wj = document.querySelector("wj")
+const hj = document.querySelector("hj")
+const content = document.querySelector("content")
+
+
+
+
+ function endFn() {
+
+    const container = document.getElementById("container")
+     
+    if(let = i; i < #text.length; i++) {
+
+    
+
+    }
+
+    alert("값을 입력해주세요");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
